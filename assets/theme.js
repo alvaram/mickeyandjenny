1df26e14d295b2878152d8c74f6ea4889f84520c
Marcello Alvarado <marsh@Marcellos-iMac.local>
2017-11-08 23:42:09 -0800

    start of slider on homepage

--- assets/theme.js
+++ assets/theme.js
@@ -2968,3 +2968,23 @@ $(theme.init);
 // Custom JS
 $( ".site-nav li" ).eq(3).addClass( "logo" );
 $( ".mobile-nav li" ).eq(3).addClass( "small--hide" );
+
+$('.slider-for').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: false,
+   fade: true,
+   asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   asNavFor: '.slider-for',
+   dots: true,
+   focusOnSelect: true
+});
+$('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   var slideno = $(this).data('slide');
+   $('.slider-nav').slick('slickGoTo', slideno - 1);
+});
