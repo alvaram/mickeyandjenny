fa13dcefe66d9a6e370d9c8b37d502bba8b20c86
Marcello Alvarado <marsh@Marcellos-iMac.local>
2017-11-10 00:30:17 -0800

    slick slider collection complete

--- assets/theme.js
+++ assets/theme.js
@@ -2969,12 +2969,60 @@ $(theme.init);
 $( ".site-nav li" ).eq(3).addClass( "logo" );
 $( ".mobile-nav li" ).eq(3).addClass( "small--hide" );
 
+$('.collection-slider-for').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+      {
+        breakpoint: 990,
+            settings: {
+              slidesToShow: 2
+            }
+      },
+      {
+        breakpoint: 540,
+            settings: {
+              slidesToShow: 1
+            }
+      }
+   ]
+});
 $('.slider-for').slick({
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   arrows: false,
-   fade: true,
-   asNavFor: '.slider-nav'
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 $('.slider-nav').slick({
    slidesToShow: 3,
